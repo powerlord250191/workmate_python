73403c730cc8428e3976ec8e0d8e5f99ecb017d5
powerlord250191 <timur-bolgov@mail.ru>
2026-05-17 22:56:45 +0300

    Сделал задание с рефакторингом

--- src/refactor/links.py
+++ src/refactor/links.py
@@ -3,34 +3,58 @@ from datetime import date
 
 from bs4 import BeautifulSoup
 
+#TODO рекомендуется добавить логирование и документирование
 
-def parse_page_links(html: str, start_date: date, end_date: date, url: str):
+
+DEFAULT_BASE_URL: str = "https://spimex.com"
+PATH_BASE: str = "/upload/reports/oil_xls/oil_xls_"
+EXTENSION: str = ".xls"
+DATE_FORMAT: str = "%Y%m%d"
+LINK_CLASS: str = "accordeon-inner__item-title link xls"
+
+
+class ParseLinkException(Exception):
+    """Исключение для ошибок парсинга ссылки"""
+    pass
+
+
+def is_valid_links(links: list[str]) -> list[str]:
+    cleaned_links: list = []
+    for link in links:
+        href: str = link.get("href").split("?")[0]
+        if not href:
+            continue
+        if not check_href(href):
+            continue
+        cleaned_links.append(link)
+    return cleaned_links
+
+
+def check_href(href: str) -> bool:
+    if PATH_BASE not in href or not href.endswith(EXTENSION):
+        return False
+    return True
+
+
+def parse_page_links(html: str, start_date: date, end_date: date):  # url зачем ?
     """
     Парсит ссылки на бюллетени с одной страницы:
     <a class="accordeon-inner__item-title link xls" href="/upload/reports/oil_xls/oil_xls_20240101_test.xls">link1</a>
     """
-    results = []
-    soup = BeautifulSoup(html, "html.parser")
-    links = soup.find_all("a", class_="accordeon-inner__item-title link xls")
+    results: list = []
+    soup: BeautifulSoup = BeautifulSoup(html, "html.parser")
+    links: list[str] = is_valid_links(soup.find_all("a", class_=LINK_CLASS))
 
     for link in links:
-        href = link.get("href")
-        if not href:
-            continue
-
-        href = href.split("?")[0]
-        if "/upload/reports/oil_xls/oil_xls_" not in href or not href.endswith(".xls"):
-            continue
-
         try:
-            date = href.split("oil_xls_")[1][:8]
-            file = datetime.datetime.strptime(date, "%Y%m%d").date()
+            date: str = link.split("oil_xls_")[1][:8]  # здесь по идее должен быть вот такой срез [0][:8]
+            file: date = datetime.datetime.strptime(date, DATE_FORMAT).date()
             if start_date <= file <= end_date:
-                u = href if href.startswith("http") else f"https://spimex.com{href}"
+                u: str = link if link.startswith("http") else f"{DEFAULT_BASE_URL}{link}"
                 results.append((u, file))
             else:
-                print(f"Ссылка {href} вне диапазона дат")
-        except Exception as e:
-            print(f"Не удалось извлечь дату из ссылки {href}: {e}")
+                print(f"Ссылка {link} вне диапазона дат")
+        except ParseLinkException as e:
+            print(f"Не удалось извлечь дату из ссылки {link}: {e}")
 
     return results
